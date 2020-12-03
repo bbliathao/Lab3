5cb9691f1fb270c2bda581ff159e60a7c2b5d726
BLIA THAO <blia451054@mail.fresnostate.edu>
2020-12-03 14:32:49 -0800

    plain english completed

--- Lab3Assignment.py
+++ Lab3Assignment.py
@@ -4,7 +4,7 @@ capture user's input 5 times for their grades
 each time we capture a user's input, we append the number to the list
 sort the list ascending, then splice the item starting at index 2
 once we have three highest number in the list, we sum them up and divide by 3
-output a message to the user
+output a message to the users
 end
 """
 
